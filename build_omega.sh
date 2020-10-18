4ce6633eff9c8568cfe336d3572801396338e0df
infected_ <infectedmushi@gmail.com>
2020-10-18 16:49:31 +0100

    original build script

--- build_omega.sh
+++ build_omega.sh
@@ -17,12 +17,12 @@ echo
 echo "Set DEFCONFIG"
 echo 
 time make O=out CC=clang omega_defconfig
-export PATH="/home/kristof/android/clang/bin:/home/kristof/android/binutils/bin:${PATH}"
-export LD_LIBRARY_PATH="/home/kristof/android/clang/lib:/home/kristof/android/clang/lib64:$LD_LIBRARY_PATH"
+export PATH="/mnt/Building/lolz_clang/bin:${PATH}"
+export LD_LIBRARY_PATH="/mnt/Building/lolz_clang/lib:/mnt/Building/lolz_clang/lib64:$LD_LIBRARY_PATH"
 
 echo
 echo "Build The Kernel"
 echo 
 
 time make -j$(nproc --all) O=out CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu-
-find /home/kristof/omega-11.0/out/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > /home/kristof/omega-11.0/out/arch/arm64/boot/dtb
+find /out/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > dtb
